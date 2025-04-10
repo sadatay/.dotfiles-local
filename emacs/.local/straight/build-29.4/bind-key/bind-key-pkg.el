679c46de260af3e53cba13f76230ea834aeedc99
Joe LaSala <joe@nowsta.com>
2025-04-10 14:35:25 -0400

    move .local to emacs dir

---
+++ emacs/.local/straight/build-29.4/bind-key/bind-key-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from bind-key.el  -*- no-byte-compile: t -*-
+(define-package "bind-key" "2.4.1.0.20240321.194020" "A simple way to manage personal keybindings" '((emacs "24.3")) :commit "7f377407b4b7d6ac9994ed983d7516bc42139885" :authors '(("John Wiegley" . "johnw@newartisans.com")) :maintainer '("John Wiegley" . "johnw@newartisans.com") :keywords '("keys" "keybinding" "config" "dotemacs" "extensions") :url "https://github.com/jwiegley/use-package")
